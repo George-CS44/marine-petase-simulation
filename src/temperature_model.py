6df6ae76b1f82f8970622c16e80e780d13dddaaa
George CS <mniffly44@gmail.com>
2026-06-23 22:05:29 +0100

    Adjust code so that values of arrhenius can be varied

--- src/temperature_model.py
+++ src/temperature_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 R = 8.314
 def arrhenius_equation(T_celsius, Ea, T_opt_celsius):
-    T_celsius = float(15)
-    Ea = float(45000)
-    T_opt_celsius = float(12)
+    T = T_celsius + 273.15
+    T_opt = T_opt_celsius + 273.15
+    return  np.exp(-Ea / (R * T)) * np.exp(Ea / (R * T_opt))
