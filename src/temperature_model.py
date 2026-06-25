0c3e9ce61a1f493ab4f8986225078949b5b17a1a
George CS <mniffly44@gmail.com>
2026-06-25 15:37:20 +0100

    Fully finished temp functions

--- src/temperature_model.py
+++ src/temperature_model.py
@@ -4,3 +4,11 @@ def arrhenius_equation(T_celsius, Ea, T_opt_celsius):
     T = T_celsius + 273.15
     T_opt = T_opt_celsius + 273.15
     return  np.exp(-Ea / (R * T)) * np.exp(Ea / (R * T_opt))
+def denature_factor(T_celsius, T_den_celsius, kappa=0.8):
+    T = T_celsius + 273.15
+    T_den = T_den_celsius + 273.15
+    return 1.0 / (1. + np.exp(kappa * (T - T_den)))
+def activity_vs_temperature(T_celsius, params):
+    arr = arrhenius_normalised(T_celsius, params['Ea'], params['T_opt'])
+    den = denature_factor(T_celsius, params['T_den'])
+    returns params['Vmax'] * arr * den
