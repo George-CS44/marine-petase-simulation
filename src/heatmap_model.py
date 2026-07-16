c590fb01277de1c148e16eeebce3eae3bebe476f
George CS <coolestboy99@outlook.com>
2026-07-16 20:44:02 +0100

    fix more typo

--- src/heatmap_model.py
+++ src/heatmap_model.py
@@ -2,7 +2,7 @@ import numpy as np
 from src.temperature_model import activity_vs_temperature
 from src.ph_model import activity_vs_ph
 
-def gen_enviromental_heatmap(T_vals, pH_vals, params): # Grid of temperature and pH values
+def gen_environmental_heatmap(T_vals, pH_vals, params): # Grid of temperature and pH values
     T_grid, pH_grid = np.meshgrid(T_vals, pH_vals)
     T_act = activity_vs_temperature(T_grid, params)
     pH_act = activity_vs_ph(pH_grid, params)
