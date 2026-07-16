50c75b02ce2be2860906abfde365d97c66f8358c
George CS <coolestboy99@outlook.com>
2026-07-16 19:12:07 +0100

    fix

--- src/heatmap_model.py
+++ src/heatmap_model.py
@@ -1,11 +1,11 @@
 import numpy as np
-from src.temperature_model import acivity_vs_temperature
-from src.ph_model import acivity_vs_ph
+from src.temperature_model import activity_vs_temperature
+from src.ph_model import activity_vs_ph
 
-def gen_enviromental_heatmap(T_vals, pH_vals, paramas): # Grid of temperature and pH values
+def gen_enviromental_heatmap(T_vals, pH_vals, params): # Grid of temperature and pH values
     T_grid, pH_grid = np.meshgrid(T_vals, pH_vals)
-    T_act = activity_vs_temperature(T_grid, paramas)
-    pH_act = activity_vs_ph(pH_grid, paramas)
+    T_act = activity_vs_temperature(T_grid, params)
+    pH_act = activity_vs_ph(pH_grid, params)
     # Normalize the activity values to the value of Vmax
     t_normal = T_act / params['Vmax']
     pH_normal = pH_act / params['Vmax']
